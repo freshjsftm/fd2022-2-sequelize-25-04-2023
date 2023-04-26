ee1723009e836a98ac079c8c60e226aea6c1c7e8
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-04-26 15:49:43 +0300

    Add crud for user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { User } = require('../models');
 
 module.exports.createUser = async (req, res, next) => {
@@ -13,8 +14,92 @@ module.exports.createUser = async (req, res, next) => {
 
 module.exports.getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({
+      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+      // order: [['email', 'DESC']],
+      // limit: 2,
+      // offset: 4,
+      //attributes: ['id', 'email', ['is_male', 'newIsMale']]
+      // where: {
+      //   [Op.and]: [
+      //     {
+      //       id: {
+      //         [Op.gt]: 7,
+      //       },
+      //     },
+      //     { isMale: false },
+      //   ],
+      // },
+    });
     res.status(200).send({ data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getUserByPk = async (req, res, next) => {
+  try {
+    const {
+      params: { idUser },
+    } = req;
+    const user = await User.findByPk(idUser, {
+      attributes: { exclude: ['password'] },
+    });
+    res.status(200).send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateUserStatic = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { idUser },
+    } = req;
+    const [, [updatedUser]] = await User.update(body, {
+      where: { id: idUser },
+      //returning: ['id', 'email']
+      returning: true,
+    });
+    updatedUser.password = undefined;
+
+    // const user = updatedUser.get();
+    // delete user.password;
+
+    res.status(200).send({ data: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateUserInstance = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { idUser },
+    } = req;
+    const userInstance = await User.findByPk(idUser);
+    const updatedUser = await userInstance.update(body, {
+      returning: true,
+    });
+    updatedUser.password = undefined;
+    res.status(200).send({ data: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteUserInstance = async (req, res, next) => {
+  try {
+    const {
+      params: { idUser },
+    } = req;
+    const user = await User.findByPk(idUser, {
+      attributes: { exclude: ['password'] },
+    });
+    await user.destroy();
+    res.status(200).send({ data: user });
   } catch (error) {
     next(error);
   }
--- router.js
+++ router.js
@@ -6,4 +6,11 @@ const router = Router();
 router.post('/users', UserController.createUser);
 router.get('/users', UserController.getAllUsers);
 
+router.get('/users/:idUser', UserController.getUserByPk)
+
+router.put('/users/:idUser/static', UserController.updateUserStatic);
+router.put('/users/:idUser/instance', UserController.updateUserInstance);
+
+router.delete('/users/:idUser/instance', UserController.deleteUserInstance);
+
 module.exports = router;
